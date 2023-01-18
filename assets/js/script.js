0dd0dd35a712ba24c5e287559a826ba859f9cb05
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2023-01-18 15:31:08 +0200

    feat: Add first section-slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+import "../../node_modules/bootstrap/js/src/carousel.js"; 
+
 const btnBurger = document.querySelector('.btn-burger');
 const navbarMobile = document.querySelector('.navbar-mobile');
 
